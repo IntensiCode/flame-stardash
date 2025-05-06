244e5121788c351142ba140b5655490faeb5bfa1
Daniel Lukic <daniel.lukic@gmx.net>
2025-05-06 11:02:08 +0200

    Simplify EnemyType

--- lib/game/base/enemy_type.dart
+++ lib/game/base/enemy_type.dart
@@ -1,32 +1,13 @@
-enum SpawnType {
-  random,
-}
-
 enum EnemyType {
   /// Jumps rapidly between different grid depths or adjacent lanes. Fires basic energy spikes.
-  Flipper(
-    first_level: 1,
-    spawn_type: SpawnType.random,
-  ),
+  Flipper,
 
   /// Moves forward until z zero. Fires basic energy spikes. Releases two flippers when destroyed or at z zero.
-  Tanker(
-    first_level: 3,
-    spawn_type: SpawnType.random,
-  ),
+  Tanker,
 
   /// Extends hazardous energy spikes across grid segments.
-  Spiker(
-    first_level: 4,
-    spawn_type: SpawnType.random,
-  ),
-  ;
+  Spiker,
 
-  const EnemyType({
-    required this.first_level,
-    required this.spawn_type,
-  });
-
-  final int first_level;
-  final SpawnType spawn_type;
+  /// Jumps like Flipper, but will also electrify the current lane randomly.
+  Pulsar(),
 }
