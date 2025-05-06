7ebb9fd71688aa11160ccce0df27dbd6f8f737ea
Daniel Lukic <daniel.lukic@gmx.net>
2025-05-06 08:51:38 +0200

    Make enemy count dynamic across levels

--- lib/game/enemies/enemies.dart
+++ lib/game/enemies/enemies.dart
@@ -1,9 +1,10 @@
 import 'package:flame/components.dart';
 import 'package:stardash/core/common.dart';
-import 'package:stardash/game/base/has_context.dart';
 import 'package:stardash/game/base/enemy_type.dart';
+import 'package:stardash/game/base/has_context.dart';
 import 'package:stardash/game/enemies/spawn_event.dart';
 import 'package:stardash/game/level/level.dart';
+import 'package:stardash/util/log.dart';
 
 extension HasContextExtensions on HasContext {
   Enemies get enemies => cache.putIfAbsent('enemies', () => Enemies());
@@ -34,15 +35,21 @@ class Enemies extends Component with HasContext {
         );
 
     // Level 1 and 2 have flippers only. Will start flipping early in level 2.
-    if (level_number <= 2) {
-      return [for (int i = 0; i < 4; i++) _flipper(i * 0.5)];
-    }
+    // Level 3 introduces tankers. But only two flippers from now on.
+    // Level 4 introduces spikers.
 
-    // Level 3 introduces tankers. But only two flippers at start.
-    if (level_number == 3) {
-      return [_flipper(0.0), _flipper(0.5), _tanker(1.0), _tanker(1.5)];
-    }
+    final flippers = level_number <= 2 ? 4 : 2;
+    final tankers = (level_number - 2).clamp(0, 4);
+    final spikers = (level_number - 3).clamp(0, 4);
+    var count = flippers + tankers + spikers;
 
-    return [_flipper(0.0), _flipper(0.25), _spiker(0.5), _tanker(0.75), _tanker(1.0)];
+    final delta = (3.0 / count).clamp(0.25, 0.5);
+    log_info('delta: $delta count: $count raw: ${3 / count}');
+
+    return [
+      for (int i = 0; i < flippers; i++) _flipper(i * delta),
+      for (int i = 0; i < tankers; i++) _tanker(2.0 + i * delta),
+      for (int i = 0; i < spikers; i++) _spiker(3.0 + i * delta),
+    ];
   }
 }
